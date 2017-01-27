54912991e6893a8387fff55e138919a0a7e61863
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2017-01-27 08:05:47 +0300

    append - 20%, package.json - auto test

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,10 +2,24 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
+    	this._head = null;
+    	this._tail = null;
     	this.length = 0;
     }
 
-    append(data) {}
+    append(data) {
+    	if(this._head == null){
+    		this._head = new Node(data);
+    		this._tail = this._head;
+    		this.length++;
+    		return
+    	}
+    	let node = new Node(data);
+    	this._tail = node;
+    	node.prev = this._head;
+    	this.length++;
+
+    }
 
     head() {}
 
